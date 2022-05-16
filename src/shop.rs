7737d75a76ed2d1470fbed3003d2d7ff3b8b50f8
Forest Anderson <forestkzanderson@gmail.com>
2022-05-16 14:34:36 -0400

    Initial commit

---
+++ src/shop.rs
@@ -0,0 +1,48 @@
+use crate::{card::Card, FightResult};
+
+pub struct Shop {
+    pub cards: Vec<Card>,
+}
+
+impl Shop {
+    /// Get the price of the most expensive card in the shop
+    pub fn most_expensive(&self) -> u32 {
+        self.cards.iter().map(|card| card.price).max().unwrap()
+    }
+
+    /// Get the total damage of all cards in the shop
+    pub fn total_damage(&self) -> u32 {
+        self.cards.iter().map(|card| card.damage).sum()
+    }
+
+    /// Get the total health of all cards in the shop
+    pub fn total_health(&self) -> u32 {
+        self.cards.iter().map(|card| card.health).sum()
+    }
+
+    /// Simulate a fight against another store. Returns a FightResult::Win if
+    /// this store wins, FightResult::Loss if this store loses, and a
+    /// FightResult::Tie if both stores win the same number of battles.
+    pub fn fight_store(&self, other: &Shop) -> FightResult {
+        let mut this_store_wins = 0;
+        let mut other_store_wins = 0;
+
+        for card1 in &self.cards {
+            for card2 in &other.cards {
+                match card1.print_fight(card2) {
+                    FightResult::Win => this_store_wins += 1,
+                    FightResult::Loss => other_store_wins += 1,
+                    _ => {}
+                }
+            }
+        }
+
+        if this_store_wins > other_store_wins {
+            FightResult::Win
+        } else if this_store_wins < other_store_wins {
+            FightResult::Loss
+        } else {
+            FightResult::Tie
+        }
+    }
+}
